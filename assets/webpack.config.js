1beabc77f5f67e6e6ec4cef80ec9dfed27ceb5c7
Farolan Faisal <farolanfaisal@gmail.com>
2020-07-28 07:25:15 +0800

    add jquery

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -40,6 +40,15 @@ module.exports = (env, options) => {
             'css-loader',
             'sass-loader',
           ],
+        },
+        {
+          test: require.resolve('jquery'),
+          use: [{
+            loader: 'expose-loader',
+            options: {
+              exposes: ['$', 'jQuery']
+            }
+          }]
         }
       ]
     },
