77fce97c99cd40a8391104642503ed44063d7858
Farolan Faisal <farolanfaisal@gmail.com>
2020-07-28 18:18:06 +0800

    update forms

--- lib/skuldaring_web/live/forms/login_form.ex
+++ lib/skuldaring_web/live/forms/login_form.ex
@@ -1,3 +1,9 @@
 defmodule SkuldaringWeb.Forms.LoginForm do
   use SkuldaringWeb, :live_component
+
+  @impl true
+  def handle_event("validate", %{"email" => _email, "password" => _password}, socket) do
+    {:noreply, socket}
+  end
+
 end
