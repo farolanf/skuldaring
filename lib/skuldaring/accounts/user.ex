7627eea25c3dfe0a5d59b56625caeb30402ea376
Farolan Faisal <farolanfaisal@gmail.com>
2020-08-02 17:40:48 +0800

    add roles

--- lib/skuldaring/accounts/user.ex
+++ lib/skuldaring/accounts/user.ex
@@ -4,9 +4,10 @@ defmodule Skuldaring.Accounts.User do
 
   schema "users" do
     field :email, :string
+    field :username, :string
     field :first_name, :string
     field :last_name, :string
-    field :username, :string
+    field :roles, {:array, :string}
 
     timestamps()
   end
@@ -14,7 +15,7 @@ defmodule Skuldaring.Accounts.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:email, :username, :first_name, :last_name])
+    |> cast(attrs, [:email, :username, :first_name, :last_name, :roles])
     |> validate_required([:email, :username, :first_name, :last_name])
   end
 end
