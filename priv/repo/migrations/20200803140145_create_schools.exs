620fe526315d68a91fa83af34b4e7dc5ae1ec01c
Farolan Faisal <farolanfaisal@gmail.com>
2020-08-03 23:12:28 +0800

    update on_delete

--- priv/repo/migrations/20200803140145_create_schools.exs
+++ priv/repo/migrations/20200803140145_create_schools.exs
@@ -4,7 +4,7 @@ defmodule Skuldaring.Repo.Migrations.CreateSchools do
   def change do
     create table(:schools) do
       add :name, :string, null: false
-      add :user_id, references(:users), null: false
+      add :user_id, references(:users, on_delete: :nothing), null: false
 
       timestamps()
     end
