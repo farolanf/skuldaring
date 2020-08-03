55b403d0b75117ff8417a0b340f5d06049c22748
Farolan Faisal <farolanfaisal@gmail.com>
2020-08-03 17:16:37 +0800

    update breadcrumbs

--- lib/skuldaring_web/live/nav_live/breadcrumbs_live.ex
+++ lib/skuldaring_web/live/nav_live/breadcrumbs_live.ex
@@ -1,17 +1,31 @@
 defmodule SkuldaringWeb.NavLive.BreadcrumbsLive do
   use SkuldaringWeb, :live_component
 
+  import Skuldaring.Utils
+
+  @paths [
+    # path regexes for breadcrumbs
+    {"/", "Beranda"},
+    {"/akun", "Akun"},
+    {"/sekolah", "Sekolah"},
+    {"/[^/]+/murid", "Murid"},
+  ]
+
   @impl true
   def preload(assign_list) do
-    IO.inspect assign_list, label: "assigns"
-    path = []
-
-    # path = [%{label: "Beranda", to: "/"} | path]
+    breadcrumbs = case assign_list do
+      [%{uri: %{path: path}}] -> get_breadcrumbs(path, @paths)
+      _ -> []
+    end
 
     List.replace_at(
       assign_list,
       0,
-      Map.merge(List.first(assign_list), %{path: path})
+      Map.merge(
+        List.first(assign_list),
+        %{path: breadcrumbs}
+      )
     )
   end
-end
+
+ end
