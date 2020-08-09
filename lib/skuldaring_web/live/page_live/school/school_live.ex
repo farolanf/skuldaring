693e7568c6456183f5470db455f98daa149be5bd
Farolan Faisal <farolanfaisal@gmail.com>
2020-08-09 13:08:20 +0800

    refactor

--- lib/skuldaring_web/live/page_live/school/school_live.ex
+++ lib/skuldaring_web/live/page_live/school/school_live.ex
@@ -8,12 +8,24 @@ defmodule SkuldaringWeb.School.SchoolLive do
   alias Skuldaring.Schools
 
   @impl true
+  def mount(_params, %{"user_id" => user_id} = session, socket) do
+    Logger.debug "live session = #{inspect session}"
+
+    search_params = %{user_id: user_id}
+
+    socket = socket
+    |> handle_session(session)
+    |> assign(:schools, Schools.find_schools(search_params))
+
+    {:ok, socket}
+  end
+
+  @impl true
   def mount(_params, session, socket) do
     Logger.debug "live session = #{inspect session}"
 
     socket = socket
     |> handle_session(session)
-    |> assign(:schools, Schools.find_schools(%{where: %{name: "TK Lontar"}}))
 
     {:ok, socket}
   end
