271aa4ba51db190e18bc944c9c786e07303b08d2
Farolan Faisal <farolanfaisal@gmail.com>
2020-08-16 13:10:42 +0800

    refactor

--- lib/skuldaring_web/live/school_live/edit.ex
+++ lib/skuldaring_web/live/school_live/edit.ex
@@ -3,7 +3,7 @@ defmodule SkuldaringWeb.SchoolLive.Edit do
 
   import Skuldaring.Permissions
 
-  alias Skuldaring.Schools
+  alias Skuldaring.{Repo, Schools}
 
   @impl true
   def mount(%{"id" => id}, %{} = session, socket) do
@@ -19,8 +19,16 @@ defmodule SkuldaringWeb.SchoolLive.Edit do
 
   @impl true
   def handle_params(params, uri, socket) do
+    %{user: user, school: school} = socket.assigns
+
     socket = socket
     |> assign(:uri, URI.parse(uri))
+
+    socket = if !allow?(user, school, "view") do
+      handle_access_denied(socket)
+    else
+      socket
+    end
 
     {:noreply, apply_action(socket.assigns.live_action, params, socket)}
   end
@@ -32,11 +40,10 @@ defmodule SkuldaringWeb.SchoolLive.Edit do
 
   defp apply_action(:room_index, _params, socket) do
     school = socket.assigns.school
-
-    rooms = Schools.list_rooms(%{where: %{school_id: school.id}})
+    |> Repo.preload(:rooms)
 
     socket
-    |> assign(:rooms, rooms)
+    |> assign(:rooms, school.rooms)
   end
 
   defp apply_action(:room_edit, %{"room_id" => room_id}, socket) do
@@ -50,16 +57,6 @@ defmodule SkuldaringWeb.SchoolLive.Edit do
       socket
       |> assign(:room, room)
     end
-  end
-
-  @impl true
-  def handle_event(event, params, socket) do
-    {:noreply, apply_event(event, params, socket)}
-  end
-
-  defp apply_event("open", %{"new-room-modal" => open}, socket) do
-    socket
-    |> assign(:new_room_modal, String.to_existing_atom(open))
   end
 
 end
--- lib/skuldaring_web/live/school_live/room/index.ex
+++ lib/skuldaring_web/live/school_live/room/index.ex
@@ -1,3 +1,13 @@
 defmodule SkuldaringWeb.SchoolLive.Room.Index do
   use SkuldaringWeb, :live_component
+
+  @impl true
+  def handle_event(event, params, socket) do
+    {:noreply, apply_event(event, params, socket)}
+  end
+
+  defp apply_event("open", %{"new-room-modal" => open}, socket) do
+    socket
+    |> assign(:new_room_modal, String.to_existing_atom(open))
+  end
 end
